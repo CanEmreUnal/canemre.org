57ec76abda3c9014950020e3edfd39feba0c7df6
canemreunal <canemreunal@hotmail.com>
2023-04-12 20:49:00 +0300

    X-Forwarded-For

--- middlewares/index.js
+++ middlewares/index.js
@@ -5,6 +5,10 @@ logger.stream = {
   write: message => logger.info(message.substring(0, message.lastIndexOf('\n')))
 };
 
+morgan.token('remote-addr', function (req) {
+  return req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+});
+
 module.exports = morgan(
   ':remote-addr - :remote-user - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
   { stream: logger.stream }
